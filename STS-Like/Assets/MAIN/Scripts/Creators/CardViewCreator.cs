5a32c5590891518aadd99231511aa8d9c78dcbf0
Gabibel15022005 <gabe83-etienne@laposte.net>
2026-08-26 06:38:44 +0200

    Basic card data system

--- STS-Like/Assets/MAIN/Scripts/Creators/CardViewCreator.cs
+++ STS-Like/Assets/MAIN/Scripts/Creators/CardViewCreator.cs
@@ -6,12 +6,15 @@ public class CardViewCreator : Singleton<CardViewCreator>
     [SerializeField] private CardView cardViewPrefab;
     [SerializeField] private float cardSpawnDuration = 0.15f;
 
-    public CardView CreateCardView(Vector3 position, Quaternion rotation)
+    public CardView CreateCardView(Card card, Vector3 position, Quaternion rotation)
     {
         CardView cardView = Instantiate(cardViewPrefab, position, rotation);
         cardView.transform.localScale = Vector3.zero;
         
         cardView.transform.DOScale(Vector3.one, cardSpawnDuration);
+
+        cardView.Setup(card);
+        
         return cardView;
     }
 }
--- STS-Like/Assets/MAIN/Scripts/Systems/TestSystem.cs
+++ STS-Like/Assets/MAIN/Scripts/Systems/TestSystem.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class TestSystem : MonoBehaviour
 {
     [SerializeField] private HandView handView;
+    [SerializeField] private CardData cardData;
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            CardView cardView = CardViewCreator.Instance.CreateCardView(transform.position, Quaternion.identity);
-            StartCoroutine(handView.AddCard(cardView));
+            Card card = new(cardData); // créer la class contenant les info de la card
+            
+            CardView cardView = CardViewCreator.Instance.CreateCardView(card ,transform.position, Quaternion.identity); // créer le visuel de la card en fonction des info dans card
+            
+            StartCoroutine(handView.AddCard(cardView)); // ajoute la carte à la main du joueur
         }
     }
 }
